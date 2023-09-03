06e1fa4757444314335dcafca16f5aa6322e6648
Anton Chepko <choptonys@gmail.com>
2023-09-03 23:24:49 +0200

    lesson 1 part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,49 @@ import { UserComponent } from './components/user/user.component';
 import { UserDetailsComponent } from './components/user-details/user-details.component';
 import { UsersComponent } from './components/users/users.component';
 import {HttpClientModule} from "@angular/common/http";
+import {RouterModule, Routes} from "@angular/router";
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { UsersPageComponent } from './pages/users-page/users-page.component';
+import { UserDetailsPageComponent } from './pages/user-details-page/user-details-page.component';
+import { HeaderComponent } from './components/header/header.component';
+import { PostsComponent } from './pages/posts/posts.component';
+
+const routes:Routes=[
+  {
+    path:'',component:MainLayoutComponent,children:[
+      {
+        path:'',redirectTo:'users',pathMatch:"full"
+      },
+      {
+        path:'users',component:UsersPageComponent,children:[
+          {
+            path:':id',component:UserDetailsPageComponent
+          }
+        ]
+      },
+      {
+        path:'posts', component:PostsComponent
+      }
+    ]
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
     UserDetailsComponent,
-    UsersComponent
+    UsersComponent,
+    MainLayoutComponent,
+    UsersPageComponent,
+    UserDetailsPageComponent,
+    HeaderComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
