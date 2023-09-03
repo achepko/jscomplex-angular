e9d279c40bef2a72ccffe37cb02d58d1db5b6ebd
Anton Chepko <choptonys@gmail.com>
2023-09-03 20:52:01 +0200

    lesson 1 part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app.component';
+import { UserComponent } from './components/user/user.component';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
+import { UsersComponent } from './components/users/users.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    UserDetailsComponent,
+    UsersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/constants/urls.ts
@@ -0,0 +1,16 @@
+const baseURL = 'https://jsonplaceholder.typicode.com'
+
+
+const users = `${baseURL}/users`
+const posts = `${baseURL}/posts`
+
+const urls = {
+  users:{
+    base:users,
+    byId:(id:number):string=>`${users}/${id}`
+  }
+}
+
+export {
+  urls
+}
