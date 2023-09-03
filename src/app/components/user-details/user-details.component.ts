358b8631f3f0bd6739bc01e2707bd49f8954dd19
Anton Chepko <choptonys@gmail.com>
2023-09-03 23:25:32 +0200

    lesson 1 part 2

---
+++ src/app/components/user-details/user-details.component.ts
@@ -0,0 +1,23 @@
+import {Component, Input} from '@angular/core';
+import {IUser} from "../../interfaces";
+import {ActivatedRoute, Router} from "@angular/router";
+import {UserService} from "../../services";
+
+@Component({
+  selector: 'app-user-details',
+  templateUrl: './user-details.component.html',
+  styleUrls: ['./user-details.component.css']
+})
+export class UserDetailsComponent {
+  user:IUser
+  constructor(private router:Router, private activatedRoute:ActivatedRoute,private userService:UserService) {
+    this.activatedRoute.params.subscribe(({id})=>{
+    let state = this.router.getCurrentNavigation()?.extras.state as IUser;
+    if (state){
+    this.user=state
+    }else{
+      this.userService.geyById(id).subscribe(value=>this.user = value)
+    }
+    })
+  }
+}
